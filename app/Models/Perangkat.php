93b56dda166d32758b5832f01806f879956c8703
ap-pamungkas <agustinusputra94@gmail.com>
2025-06-23 13:05:12 +0700

    refactor(Perangkat): clean up model by removing unused imports and formatting
    Remove unused Str and QrCode imports and fix inconsistent whitespace formatting

--- app/Models/Perangkat.php
+++ app/Models/Perangkat.php
@@ -3,29 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class Perangkat extends Model
 {
     protected  $table = 'perangkat';
 
     protected $fillable = [
-
         'no_seri',
         'kondisi',
-
-
     ];
 
 
-
-
-
-     public function logPerangkat()
+    public function logPerangkat()
     {
         return $this->hasMany(PetugasInsiden::class, 'id_perangkat', 'id');
     }
-
-
 }
